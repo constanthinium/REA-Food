42c2f7d1b5f46c478f0eb468c971c8dd62aba7af
konstantin <isip_k.a.dzuba@mpt.ru>
2022-05-30 14:38:32 +0300

    Restaurants image

--- app/src/main/java/ru/rea/food/ui/item/RestaurantItem.kt
+++ app/src/main/java/ru/rea/food/ui/item/RestaurantItem.kt
@@ -1,27 +1,26 @@
 package ru.rea.food.ui.item
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import ru.rea.food.ImageFinder
 import ru.rea.food.Place
-import ru.rea.food.REAFoodService
 
 @ExperimentalMaterialApi
 @Composable
-fun RestaurantItem(it: Place, onClick: () -> Unit) {
+fun RestaurantItem(place: Place, onClick: () -> Unit) {
+    var image by remember { mutableStateOf<String?>(null) }
+    ImageFinder.find(place.name) { image = it }
+
     Card(
         modifier = Modifier.padding(16.dp),
         elevation = 4.dp,
@@ -36,12 +35,13 @@ fun RestaurantItem(it: Place, onClick: () -> Unit) {
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             AsyncImage(
-                model = "${REAFoodService.baseUrl}/${it.photoUrl}",
-                contentDescription = null
+                model = image,
+                contentDescription = null,
+                modifier = Modifier.size(192.dp)
             )
-            Text(text = it.name, fontWeight = FontWeight.Bold)
-            Text(text = "Работает с ${it.openTime} до ${it.closeTime}")
-            if (it.operational) {
+            Text(text = place.name, fontWeight = FontWeight.Bold)
+            Text(text = "Работает с ${place.openTime} до ${place.closeTime}")
+            if (place.operational) {
                 Text(text = "Работает", color = Color.Green)
             } else {
                 Text(text = "Не работает", color = Color.Red)
