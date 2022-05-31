c8bc300db9dfaeedcc7f427621a2f1cbf5d13037
konstantin <isip_k.a.dzuba@mpt.ru>
2022-05-31 23:54:36 +0300

    Suppress view model creation warning

--- app/src/main/java/ru/rea/food/vm/MenuViewModelFactory.kt
+++ app/src/main/java/ru/rea/food/vm/MenuViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class MenuViewModelFactory(val token: String) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
         return MenuViewModel(token) as T
     }
 }
